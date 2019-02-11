b166604c3111e23621c3f44fbd2ab044453de879
yasuda <tasssra@gmail.com>
2019-02-12 07:40:45 +0900

    add 16.js

---
+++ js_code_recipe/16.js
@@ -0,0 +1,13 @@
+const myFruit = 'apple';
+
+switch (myFruit) {
+    case 'apple':
+        alert('apple');
+        break;
+    case 'orange':
+        alert('apple');
+        break;
+    default:
+        alert('others');
+        break;
+}
