2281b536b13cad0e10e41bb6699c50968ef15cb3
yasuda <tasssra@gmail.com>
2019-02-14 07:15:47 +0900

    add 22.js

---
+++ js_code_recipe/22.js
@@ -0,0 +1,12 @@
+/**
+ * 四捨五入、切り捨て、切り上げ、整数部分を返す
+ * Math.round(数値) => 四捨五入
+ * Math.floor(数値) => 切り捨てる
+ * Math.ceil(数値) => 切り上げる
+ * Math.trunc(数値) => 数値の整数部分を返す
+ */ 
+
+Math.round(6.24); // 6
+Math.floor(6.24); // 6
+Math.ceil(6.24); // 7
+Math.trunc(6.24); // 6
